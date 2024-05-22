e0047cd07106bf93bb1f764f43f15cbab719147f
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-22 15:47:43 -0300

    refactor: 💡 move force new deployment to ecr pipeline

--- deploy/create_ecr.py
+++ deploy/create_ecr.py
@@ -2,13 +2,19 @@ from scripts.typescript import Typescript
 from scripts.cloudformation import CloudFormation
 from scripts.args import get_args
 from stacks import ecr
+from stacks.ecs import my_stack_name as ecs_stack_name
 from scripts.exception import DeployException
 from scripts.docker import Docker
+from scripts.ecs import ECS
 
 args = get_args(
     {
         "stage": {"type": "str", "required": False, "default": "prod"},
-        "microservice": {"type": "str", "required": False, "default": "contact-bridges"},
+        "microservice": {
+            "type": "str",
+            "required": False,
+            "default": "contact-bridges",
+        },
         "tenant": {"type": "str", "required": False, "default": "tcc"},
         "region": {"type": "str", "required": False, "default": "us-east-2"},
         "profile": {"type": "str", "required": False, "default": "default"},
@@ -40,5 +46,12 @@ if not cloudformation.stack_is_succesfully_deployed(stack_name=ECR_STACK["stack_
 
 ecr_uri = docker.ecr_uri(account_id=account_id, region=region)
 typescript.build()
-image=f"{stage}-{tenant}-{microservice}"
-docker.build_and_push(ecr_uri=ecr_uri,region=region, image=image, tag="latest")
+image = f"{stage}-{tenant}-{microservice}"
+docker.build_and_push(ecr_uri=ecr_uri, region=region, image=image, tag="latest")
+
+
+ecs_stack_name_ = ecs_stack_name(stage=stage, tenant=tenant)
+ecs = ECS(profile=profile, region=region, log_level=log_level)
+ecs.force_stack_new_deployment(
+    cloudformation=cloudformation, stack_name=ecs_stack_name_
+)
--- deploy/create_ecs.py
+++ deploy/create_ecs.py
@@ -2,12 +2,15 @@ from scripts.cloudformation import CloudFormation
 from scripts.args import get_args
 from stacks import ecs
 from scripts.exception import DeployException
-from scripts.ecs import ECS
 
 args = get_args(
     {
         "stage": {"type": "str", "required": False, "default": "prod"},
-        "microservice": {"type": "str", "required": False, "default": "contact-bridges"},
+        "microservice": {
+            "type": "str",
+            "required": False,
+            "default": "contact-bridges",
+        },
         "tenant": {"type": "str", "required": False, "default": "tcc"},
         "region": {"type": "str", "required": False, "default": "us-east-2"},
         "profile": {"type": "str", "required": False, "default": "default"},
@@ -18,7 +21,7 @@ args = get_args(
         "scale_in_cooldown": {"type": "int", "required": False, "default": 60},
         "cpu_utilization": {"type": "int", "required": False, "default": 40},
         "log_level_compute": {"type": "str", "required": False, "default": "debug"},
-        "default_email": { "type": "str", "required": True }
+        "default_email": {"type": "str", "required": True},
     }
 )
 
@@ -36,7 +39,9 @@ cloudformation = CloudFormation(profile=profile, region=region, log_level=log_le
 # 🚀 ECS
 ################################################
 exports = cloudformation.list_exports()
-key_arn = cloudformation.get_export_value(exports=exports, name=f"{stage}-{tenant}-encrypt-key-arn")
+key_arn = cloudformation.get_export_value(
+    exports=exports, name=f"{stage}-{tenant}-encrypt-key-arn"
+)
 ECS_STACK = ecs.stack(
     stage=stage,
     tenant=tenant,
@@ -48,19 +53,9 @@ ECS_STACK = ecs.stack(
     scale_in_cooldown=args["scale_in_cooldown"],
     cpu_utilization=args["cpu_utilization"],
     key_arn=key_arn,
-    default_email=default_email
+    default_email=default_email,
 )
 
 cloudformation.deploy_stack(stack=ECS_STACK)
-ecs = ECS(profile=profile, region=region, log_level=log_level)
 if not cloudformation.stack_is_succesfully_deployed(stack_name=ECS_STACK["stack_name"]):
     raise DeployException(stack=ECS_STACK)
-
-stack_resources = cloudformation.describe_stack_resources(stack_name=ECS_STACK["stack_name"])
-print(stack_resources)
-for resource in stack_resources["StackResources"]:
-    if resource["LogicalResourceId"] == "Service":
-        service = resource["PhysicalResourceId"]
-        break
-
-ecs.force_new_deployment(cluster=f"{stage}-{tenant}-{microservice}-cluster",service=service)
